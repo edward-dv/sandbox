48ced4c5b193b75bc41f76859034113958ff7306
Edward De Veer <edvp93@gmail.com>
2021-03-16 00:00:47 +0000

    QoL improvements

--- models/checkout_reporting/datamarts/users_pageviews.sql
+++ models/checkout_reporting/datamarts/users_pageviews.sql
@@ -4,23 +4,21 @@
 
 WITH 
 prep AS (
-    SELECT 
-        pv.user_id
+    SELECT
+        pv.pageview_id 
+        , pv.user_id
         , pv.page_url
         , pv.pageview_datetime
-        -- Results in the postcode the user was in at the time of the page view
-        , CASE 
-            WHEN CAST(pv.pageview_datetime AS DATE) >= u.postcode_valid_from 
-             AND CAST(pv.pageview_datetime AS DATE) < u.postcode_valid_to 
-            THEN u.postcode
-        END                         AS postcode
+        , u.postcode
         , u.postcode_index
-    FROM {{ ref('pageviews') }}     AS pv
-    JOIN {{ ref('users') }}         AS u
+    FROM {{ ref('fct_pageviews') }} AS pv
+    JOIN {{ ref('dim_users') }}     AS u
       ON u.user_id = pv.user_id
+      -- Gets only the postcode the user was in at the time of the pageview
+     AND CAST(pv.pageview_datetime AS DATETIME) >= CAST(u.postcode_valid_from AS DATETIME)
+     AND CAST(pv.pageview_datetime AS DATETIME) < CAST(u.postcode_valid_to AS DATETIME)
 )
 
 SELECT * 
   FROM prep 
- WHERE postcode IS NOT NULL
- ORDER BY 1
+ ORDER BY 2, 4
--- models/checkout_staging/pageviews/staging_pageviews.sql
+++ models/checkout_staging/pageviews/staging_pageviews.sql
@@ -2,8 +2,14 @@
     materialized ="incremental"
 ) }}
 
-SELECT *
+SELECT 
+  {{ dbt_utils.surrogate_key(['user_id','page_url','pageview_datetime' ]) }} AS pageview_id
+  , user_id
+  , page_url
+  , pageview_datetime
+  , load_timestamp
 FROM {{ source('checkout_raw','pageviews_extract') }}
+
 {% if is_incremental() %}
 
   WHERE load_timestamp > (SELECT max(load_timestamp) FROM {{ this }})
